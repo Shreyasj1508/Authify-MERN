914e068988de2aa3f9677c4963c206a046b08b5e
Shreyas <shreyasjaiswal1515@gmail.com>
2025-04-02 19:05:36 +0530

    final commit

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import reactDOM from 'react-dom/client'
+
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+reactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Toaster position="top-right" />
+    <App />
+  </BrowserRouter>
+);
